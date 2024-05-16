872fd31718d8257e587aba2c91e3dc62cb9028b4
Jack Jiang <jjiang40@stuy.edu>
2024-05-16 12:29:50 -0400

    trying to use arrow to track the rotation

--- Beyblade.py
+++ Beyblade.py
@@ -16,16 +16,30 @@ I = 3 * M * (radius**2) /10
 α = 0
 ω = 0
 L = I*ω
-t=0; dt=3600
+t=0; dt=0.5
 leave = True
-
+# arrow(pos=vec(0,0,0),axis=vec(0,0,1),color=color.orange)
 def leaveLoop():
     global leave 
     leave = not(leave)
+beyblade.omega = 0*vector(0,0,0)
 
 endButton = button(bind=leaveLoop,text="Click me to stop rotating!")
-
-while (leave):
-    beyblade.rotate(axis=vec(0,1,0),angle=pi/4)
+force_vector = arrow(pos=vec(0,0,-1))
+while (t<100 or leave):
     rate(1)
+    force = vector(0,0,cos(t))
+    force_location = vector(0,0,radius)
+    # Update force visual.
+    force_vector.pos = force_location + vector(0,0,.5)
+    force_vector.axis=force
+    # Calculate torque.
+    torque = cross(force_location,force)
+    # Use update procedure.
+    beyblade.omega = beyblade.omega + torque/I*dt
+    beyblade.rotate(angle=mag(beyblade.omega)*dt,axis=vec(0,1,0))
+    # Update time
+    t = t + dt
+    # beyblade.rotate(axis=vec(0,1,0),angle=pi/4)
+
 
