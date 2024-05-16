93ad9b9f35be93759757092e3cd9a671a95e6fb7
Jack Jiang <jjiang40@stuy.edu>
2024-05-16 17:34:45 -0400

    changed back to old code

--- Beyblade.py
+++ Beyblade.py
@@ -16,34 +16,18 @@ PE = 0
 KE = 0
 friction = 0
 I = 3 * M * (radius**2) /10
-α = 0
 ω = 0
 L = I*ω
-t=0; dt=0.5
+t=0; dt=3600
 leave = True
-# arrow(pos=vec(0,0,0),axis=vec(0,0,1),color=color.orange)
+
 def leaveLoop():
     global leave 
     leave = not(leave)
-beyblade.omega = 0*vector(0,0,0)
 
 endButton = button(bind=leaveLoop,text="Click me to stop rotating!")
-force_vector = arrow(pos=vec(0,0,-1))
-while (t<100 and leave):
-    spin.rotate(axis=vec(0,1,0),angle=pi/360)
-    rate(1)
-    # force = vector(0,0,cos(t))
-    # force_location = vector(0,0,radius)
-    # # Update force visual.
-    # force_vector.pos = force_location + vector(0,0,.5)
-    # force_vector.axis=force
-    # # Calculate torque.
-    # torque = cross(force_location,force)
-    # # Use update procedure.
-    # beyblade.omega = beyblade.omega + torque/I*dt
-    # beyblade.rotate(angle=mag(beyblade.omega)*dt,axis=vec(0,1,0))
-    # # Update time
-    # t = t + dt
-    # # beyblade.rotate(axis=vec(0,1,0),angle=pi/4)
 
+while (leave):
+    spin.rotate(axis=vec(0,1,0),angle=pi/360)
+    rate(1000)
 
